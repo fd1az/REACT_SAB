f3be8dc3c4630228fce1eb9f4c3b19eb22b5b0b9
facu diaz <facu.diaz777@gmail.com>
2020-10-17 14:55:20 -0300

    final clase 1 react para todes

--- clase_1_a/src/App.js
+++ clase_1_a/src/App.js
@@ -1,1 +1,29 @@
-const lala = '';
+import React from 'react';
+import { render } from 'react-dom';
+
+const Cars = ({ marca, modelo, tipo }) => {
+  return (
+    <div>
+      <h2>{marca}</h2>
+      <h3>{modelo}</h3>
+      <h4>{tipo}</h4>
+    </div>
+  );
+};
+
+const Title = ({ title }) => {
+  return <h1 id="miId">{title}</h1>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <Title title="React baby" />
+      <Cars marca="FIAT" modelo="Punto" tipo="sedan" />
+      <Cars marca="FORD" modelo="FIESTA" tipo="sedan" />
+      <Cars marca="FERRARI" modelo="F50" tipo="coupé" />
+    </div>
+  );
+};
+
+render(React.createElement(App), document.getElementById('root'));
